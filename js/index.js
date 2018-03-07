2076c2397729f8a04defddcd18a982de34e20c7b
ClayMurphree <35501717+ClayMurphree@users.noreply.github.com>
2018-03-07 15:44:01 -0600

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,4 @@
 // my js file
-
-$(document).ready(function() {
-
 //create a loop to "print out" all elements in the DOM"
 //1. create the loop structure. How to figure out the LENGTH, i.e., the number of iterations of my loop
 //2. hoow do i access an element from my page... then ALL elements?
@@ -14,4 +11,3 @@ for ( i=0; i < allDOMElements.length; i++) {
 }
 
 
-});
